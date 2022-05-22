c1dd94fd676f05a7bce72cbef1132d2fed443992
Sarah86 <sarah@DESKTOP-VI58DL9.localdomain>
2022-05-22 12:20:25 +0200

    readme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,214 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect, useRef } from "react";
+import "./App.css";
+import * as d3 from "d3";
+import flare from "./assets/flare-2.json";
+import flare02 from './assets/flare02.json';
+
+// Copyright 2021 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/tree
+function Tree(
+  data,
+  {
+    // data is either tabular (array of objects) or hierarchy (nested objects)
+    path, // as an alternative to id and parentId, returns an array identifier, imputing internal nodes
+    id = Array.isArray(data) ? (d) => d.id : null, // if tabular data, given a d in data, returns a unique identifier (string)
+    parentId = Array.isArray(data) ? (d) => d.parentId : null, // if tabular data, given a node d, returns its parent’s identifier
+    children, // if hierarchical data, given a d in data, returns its children
+    tree = d3.tree, // layout algorithm (typically d3.tree or d3.cluster)
+    sort, // how to sort nodes prior to layout (e.g., (a, b) => d3.descending(a.height, b.height))
+    label, // given a node d, returns the display name
+    title, // given a node d, returns its hover text
+    link, // given a node d, its link (if any)
+    linkTarget = "_blank", // the target attribute for links (if any)
+    width = 640, // outer width, in pixels
+    height, // outer height, in pixels
+    r = 3, // radius of nodes
+    padding = 1, // horizontal padding for first and last column
+    fill = "#999", // fill for nodes
+    fillOpacity, // fill opacity for nodes
+    stroke = "#555", // stroke for links
+    strokeWidth = 1.5, // stroke width for links
+    strokeOpacity = 0.4, // stroke opacity for links
+    strokeLinejoin, // stroke line join for links
+    strokeLinecap, // stroke line cap for links
+    halo = "#fff", // color of label halo
+    haloWidth = 3, // padding around the labels
+  } = {}
+) {
+  // If id and parentId options are specified, or the path option, use d3.stratify
+  // to convert tabular data to a hierarchy; otherwise we assume that the data is
+  // specified as an object {children} with nested objects (a.k.a. the “flare.json”
+  // format), and use d3.hierarchy.
+  const root =
+    path != null
+      ? d3.stratify().path(path)(data)
+      : id != null || parentId != null
+      ? d3.stratify().id(id).parentId(parentId)(data)
+      : d3.hierarchy(data, children);
+
+  // Sort the nodes.
+  if (sort != null) root.sort(sort);
+
+  // Compute labels and titles.
+  const descendants = root.descendants();
+  const L = label == null ? null : descendants.map((d) => label(d.data, d));
+
+  // Compute the layout.
+  const dx = 10;
+  const dy = width / (root.height + padding);
+  tree().nodeSize([dx, dy])(root);
+
+  // Center the tree.
+  let x0 = Infinity;
+  let x1 = -x0;
+  root.each((d) => {
+    if (d.x > x1) x1 = d.x;
+    if (d.x < x0) x0 = d.x;
+  });
+
+  // Compute the default height.
+  if (height === undefined) height = x1 - x0 + dx * 2;
+
+  const svg = d3
+    .create("svg")
+    .attr("viewBox", [(-dy * padding) / 2, x0 - dx, width, height])
+    .attr("width", width)
+    .attr("height", height)
+    .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 10);
+
+  svg
+    .append("g")
+    .attr("fill", "none")
+    .attr("stroke", stroke)
+    .attr("stroke-opacity", strokeOpacity)
+    .attr("stroke-linecap", strokeLinecap)
+    .attr("stroke-linejoin", strokeLinejoin)
+    .attr("stroke-width", strokeWidth)
+    .selectAll("path")
+    .data(root.links())
+    .join("path")
+    .attr(
+      "d",
+      d3
+        .linkHorizontal()
+        .x((d) => d.y)
+        .y((d) => d.x)
+    );
+
+  //Get the colors for more than one occurence
+  const grouped_data = d3.group(descendants, (d) => d.data.name);
+  let duplicatedValues = [];
+  grouped_data.forEach((value, key, map) => {
+    const valor = value.length > 1 ? key : null;
+    if (valor)
+    duplicatedValues.push({
+        name: valor,
+        value: value.length,
+      });
+  });
+
+  const color = (d = { data: { name: "Vimalamitra" } }) => {
+    const node = duplicatedValues.find((node) => d.data.name === node.name);
+    const value = node?.value;
+    const colorIndex = value - 1
+    return node ? d3.schemeSet2[colorIndex] : stroke;
+  };
+
+  const node = svg
+    .append("g")
+    .selectAll("a")
+    .data(root.descendants())
+    .join("a")
+    .attr("xlink:href", link == null ? null : (d) => link(d.data, d))
+    .attr("target", link == null ? null : linkTarget)
+    .attr("transform", (d) => `translate(${d.y},${d.x})`);
+
+  node
+    .append("circle")
+    .attr("fill", (d) => (d.children ? stroke : fill))
+    .attr("r", r);
+
+  if (title != null) node.append("title").text((d) => title(d.data, d));
+
+  if (L)
+    node
+      .append("text")
+      .attr("dy", "0.32em")
+      .attr("x", (d) => (d.children ? -6 : 6))
+      .attr("text-anchor", (d) => (d.children ? "end" : "start"))
+      .attr("paint-order", "stroke")
+      .attr("stroke", halo)
+      .attr("stroke-width", haloWidth)
+      .attr("fill", (d) => color(d))
+      .text((d, i) => L[i]);
+
+      node
+      .append("text")
+      .attr("dy", "0.32em")
+      .attr("y", (d) => 0)
+      .attr("x", (d) => (d.children ? 25 : -25))
+      .attr("text-anchor", (d) => "start")
+      .attr("paint-order", "stroke")
+      .attr("stroke", halo)
+      .attr("stroke-width", haloWidth)
+      .attr("fill", (d) => color(d))
+      .text((d, i) => d.data.year);
+
+  return svg.node();
+}
+
+const chart = Tree(flare, {
+  label: (d) => d.name,
+  title: (d, n) =>
+    `${n
+      .ancestors()
+      .reverse()
+      .map((d) => d.data.name)
+      .join(" - ")}`, // hover tex
+  link: (d, n) => d?.link,
+  sort: (a, b) => d3.descending(a.year, b.year),
+  width: 2000,
+  height: 500,
+  margin: 300,
+});
+
+const chart02 = Tree(flare02, {
+  label: (d) => d.name,
+  title: (d, n) =>
+    `${n
+      .ancestors()
+      .reverse()
+      .map((d) => d.data.name)
+      .join(" - ")}`, // hover tex
+  link: (d, n) => d?.link,
+  sort: (a, b) => d3.descending(a.year, b.year),
+  width: 2000,
+  height: 500,
+  margin: 300,
+});
 
 function App() {
+  const svg = useRef(null);
+  const svg02 = useRef(null);
+  useEffect(() => {
+    if (svg.current) {
+      svg.current.appendChild(chart);
+    }
+    if (svg02.current) {
+      svg02.current.appendChild(chart02);
+    }
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Tibetan Buddhism Teacher - Students</h1>
+        <div ref={svg} />
+        <h1>Tibetan Buddhism Emanations</h1>
+        <div ref={svg02} />
       </header>
     </div>
   );
